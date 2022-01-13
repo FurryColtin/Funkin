29f48b184a724ea4214f25142931ae7580ba4d74
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2022-01-13 12:19:07 -0500

    story menu changed
    le epic changes

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -24,35 +24,25 @@ class StoryMenuState extends MusicBeatState
 
 	var weekData:Array<Dynamic> = [
 		['Tutorial'],
-		['Bopeebo', 'Fresh', 'Dadbattle'],
-		['Spookeez', 'South', "Monster"],
-		['Pico', 'Philly', "Blammed"],
-		['Satin-Panties', "High", "Milf"],
-		['Cocoa', 'Eggnog', 'Winter-Horrorland'],
-		['Senpai', 'Roses', 'Thorns']
+		['Lounge', 'Cutey', 'Girl', 'Break-In', 'Lied-Too'],
+		['Work-Break', 'Withdraw-Moola', 'Shifts-Over', 'Nighttime-Robbery']
 	];
-	var curDifficulty:Int = 1;
+	var curDifficulty:Int = 2;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true];
+	public static var weekUnlocked:Array<Bool> = [true, true, true];
 
 	var weekCharacters:Array<Dynamic> = [
 		['dad', 'bf', 'gf'],
 		['dad', 'bf', 'gf'],
-		['spooky', 'bf', 'gf'],
-		['pico', 'bf', 'gf'],
-		['mom', 'bf', 'gf'],
-		['parents-christmas', 'bf', 'gf'],
-		['senpai', 'bf', 'gf']
+		['dad', 'bf', 'gf']
+	//      ['worker', 'fbf', 'fgf'] //BTW This is for 2.0!!
 	];
 
 	var weekNames:Array<String> = [
-		"",
-		"Daddy Dearest",
-		"Spooky Month",
-		"PICO",
-		"MOMMY MUST MURDER",
-		"RED SNOW",
-		"hating simulator ft. moawling"
+		"The epic tutorial",
+		"Girls only lounge ft. carihyper!",
+	        "Furry only bank ft. dylan the hyper"
+//	        "Furry only house ft. furry coltin"
 	];
 
 	var txtWeekTitle:FlxText;
